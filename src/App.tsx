1079335a557ebf02ef10a2ea04235e9a38b27178
Ishei5 <86805041+Ishei5@users.noreply.github.com>
2024-08-02 19:55:36 +0300

    Price and WhyAs layouts (#1)
    Co-authored-by: Illia Pankov <illia.pankov@globallogic.com>
    Co-authored-by: Rooman <trubintolik@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
-import {Header} from "@/components/header.component.tsx";
-import {Hero} from "@/components/hero.component.tsx";
-import {Benefits} from "@/components/benefits.component"
+import { Header } from "@/components/header.component.tsx";
+import { Hero } from "@/components/hero.component.tsx";
+import { Price } from "@/components/price.component.tsx";
+import { WhyUs } from "@/components/why-us.component";
+import { Benefits } from "@/components/benefits.component";
 import { Features } from "./components/features.component";
 import { CTA } from "./components/cta.component";
 
-
 function App() {
-
   return (
-    <div className={''}>
-        <Header/>
-        <Hero/>
-        <Benefits />
-        <Features />
-        <CTA />
+    <div className={"p-4 max-w-screen-xl mx-auto"}>
+      <Header />
+      <Hero />
+      <Benefits />
+      <Price />
+      <Features />
+      <WhyUs />
+      <CTA />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
