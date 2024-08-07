d6a471c72670f92cc6adbec90fae4ace9f9cf794
Ishei5 <86805041+Ishei5@users.noreply.github.com>
2024-08-07 18:40:18 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  base: "/routerra-landing/",
   plugins: [react()],
   resolve: {
     alias: {
