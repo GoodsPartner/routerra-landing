d8304388a1f044b9ea9d4cf7ea371ff2d7357d3a
Ishei5 <86805041+Ishei5@users.noreply.github.com>
2024-10-21 17:31:38 +0300

    Add i18n (#5)
    * Add i18n
    
    * Typo fix

--- src/components/header.component.tsx
+++ src/components/header.component.tsx
@@ -1,26 +1,30 @@
-import { Mail} from "lucide-react";
-import {Logo} from "@/components/logo.component.tsx";
-
+import { Mail } from 'lucide-react';
+import { Logo } from '@/components/logo.component.tsx';
+import { useTranslation } from 'react-i18next';
+import { LanguageSwitcher } from './language-switcher.component';
 
 export const Header = () => {
-    return (
+    const { t } = useTranslation();
 
+    return (
         <header className={'flex items-center justify-between p-4 md:py-8 md:px-10'}>
-            <Logo/>
+            <Logo />
 
             <div className={'flex items-center gap-x-6'}>
-                <a href="mailto:info@routerra.io"
-                    className={'text-slate-900 border rounded-full border-slate-900  p-[11px]'}>
-                    <Mail className={'size-4 md:size-6'}/>
+                <a
+                    href="mailto:info@routerra.io"
+                    className={'text-slate-900 border rounded-full border-slate-900  p-[11px]'}
+                >
+                    <Mail className={'size-4 md:size-6'} />
                 </a>
-                {/*<a href="#"*/}
-                {/*   className={'text-slate-900 border rounded-full border-slate-900 p-[11px]'}>*/}
-                {/*    <Globe className={'size-4 md:size-6'}/>*/}
-                {/*</a>*/}
-                <a href="https://app.routerra.io"
-                   className={'hidden bg-slate-900 text-white rounded-3xl px-8 py-3 font-semibold md:block'}>Sign
-                    in</a>
+                <LanguageSwitcher />
+                <a
+                    href="https://app.routerra.io"
+                    className={'hidden bg-slate-900 text-white rounded-3xl px-8 py-3 font-semibold md:block'}
+                >
+                    {t('sign-in')}
+                </a>
             </div>
         </header>
-    )
-}
+    );
+};
